cb4cb579498b8caebab255fae07e0c423ed3fb23
vobaolan <2500116096@vanlangsaigon.edu.vn>
2026-07-29 13:25:47 +0700

    Disable eslint and ts errors on build

--- next.config.mjs
+++ next.config.mjs
@@ -37,6 +37,12 @@ const nextConfig = {
       },
     ];
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
